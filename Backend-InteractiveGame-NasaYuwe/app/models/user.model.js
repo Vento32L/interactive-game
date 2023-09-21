fdc43c2471d6d4227d0ce567535e90ec1ab0e848
Vento22L <dionisiorockkivn@gmail.com>
2023-09-20 23:23:49 -0500

    update

--- Backend-InteractiveGame-NasaYuwe/app/models/user.model.js
+++ Backend-InteractiveGame-NasaYuwe/app/models/user.model.js
@@ -15,20 +15,13 @@ var Models = {
     getUser: (data, callback) => {
         console.log("el id: ", data)
         if(dbconnection){
-            let sql
-        }
-    }
-
-    getOneUser_role: (data, callback) => {
-        console.log("el id: ", data)
-        if (dbconnection){
-            let sql = 'select * from user_role where user_ro_id = ${dbconnection.escape(data)}'
-            dbconnection.query(sql, (error, rows) => { 
+            let sql = 'select *from user where user_id = ${dbconnection.escape(data)}'
+            dbconnection.query(sql, (error, rows) => {
                 if(error) throw error
                 callback(rows)
             })
         }
-    },
+    }
 }
 
 module.exports = Models
