3c2cc561d72d6f1d40c00ada979cfc6e836ae84b
Vento22L <dionisiorockkivn@gmail.com>
2023-09-23 18:35:08 -0500

    update

---
+++ Backend-InteractiveGame-NasaYuwe/app/models/words.model.js
@@ -0,0 +1,42 @@
+const dbconnection = require('../../database/dbconnection')
+
+var Models = {
+    getWords: (callback) => {
+        if(dbconnection){
+            let sql = `select * from words`
+
+            dbconnection.query(sql, (error, rows) => {
+                if(error) throw error
+                callback(rows);
+            })
+        }
+    },
+
+    getOneWords: (data, callback) => {
+        console.log("el id: ", data)
+        if(dbconnection){
+            let sql = `select *from words where word_id = ${dbconnection.escape(data)}`
+            dbconnection.query(sql, (error, rows) => {
+                if(error) throw error
+                callback(rows)
+            })
+        }
+    },
+
+    addWords: (data, callback) => {
+        if(dbconnection){
+            let sql = `insert into words (word_description, word_sound, word_play, word_spanish_meaning, word_graph) values (${dbconnection.escape(data.word_description)},
+            ${dbconnection.escape(data.word_sound)},
+            ${dbconnection.escape(data.word_play)},
+            ${dbconnection.escape(data.word_spanish)},
+            ${dbconnection.escape(data.word_graph)})`
+
+            dbconnection.query(sql, (error, rows) =>{
+                if(error) throw error
+                callback({message : 'Usuario creado satisfactoriamente'})
+            })
+        }
+    }
+}
+
+module.exports = Models
