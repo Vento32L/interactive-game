77e06e7016fe4f1f2bfb765d6788d6f55850e0ed
Vento22L <dionisiorockkivn@gmail.com>
2023-09-24 21:50:53 -0500

    update

--- Backend-InteractiveGame-NasaYuwe/app/models/game_type_has_game.model.js
+++ Backend-InteractiveGame-NasaYuwe/app/models/game_type_has_game.model.js
@@ -1,9 +1,9 @@
 const dbconnection = require('../../database/dbconnection')
 
 var Models = {
-    getGame_has_Game_Type: (callback) => {
+    getGame_Type_has_Game: (callback) => {
         if(dbconnection){
-            let sql = `select * from game_has_game_type`
+            let sql = `select * from game_type_has_game`
 
             dbconnection.query(sql, (error, rows) => {
                 if(error) throw error
