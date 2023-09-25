14962e7194e805b867f2c2043850dd33bbb52d10
Vento22L <dionisiorockkivn@gmail.com>
2023-09-24 21:43:03 -0500

    update

--- Backend-InteractiveGame-NasaYuwe/app/routes/game_has_game_type.route.js
+++ Backend-InteractiveGame-NasaYuwe/app/routes/game_has_game_type.route.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const route = require('express').Router()
 
-const {getGame_has_Game_Type} = require('../controllers/game_has_game_type.controller')
+const {getGame_has_Game_Type} 
+ = require('../controllers/game_has_game_type.controller')
 
 route.route('/game_has_game_type').get(getGame_has_Game_Type)
