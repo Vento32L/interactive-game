4894fc9fdf206b5159cc0c8e25f5014f8c28587f
Vento22L <dionisiorockkivn@gmail.com>
2023-09-24 23:58:10 -0500

    update

--- Backend-InteractiveGame-NasaYuwe/database/dbconnection.js
+++ Backend-InteractiveGame-NasaYuwe/database/dbconnection.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+
 const myConn = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -6,11 +7,15 @@ const myConn = mysql.createConnection({
     database: "interactivegame_nasayuwe"
 })
 
+//conectar a la base datos MySQL
 myConn.connect((err)=>{
-    if(err) throw err
+    if(err){
+        console.error('Error al conectar a la base de datos' + err.message);
+        return;
+    }
     console.log('conexion a la base de datos establecida')
 })
 
 module.exports = myConn
 
-connection.end()
+myConn.end()
