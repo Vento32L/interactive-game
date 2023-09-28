32e22938094dbcb15fbb291e13b4a9a1c84f39d4
Vento22L <dionisiorockkivn@gmail.com>
2023-09-27 22:55:37 -0500

    update

--- Backend-InteractiveGame-NasaYuwe/database/dbconnection.js
+++ Backend-InteractiveGame-NasaYuwe/database/dbconnection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const myConn = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "182215",
+    password: "76A*6uyrT%",
     database: "interactivegame_nasayuwe"
 })
 
