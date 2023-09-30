cac47c75c4a16af80a3e1d85a4c2b1a205e08fb4
Vento22L <dionisiorockkivn@gmail.com>
2023-09-29 20:26:37 -0500

    update

--- Backend-InteractiveGame-NasaYuwe/app/models/user_role.model.js
+++ Backend-InteractiveGame-NasaYuwe/app/models/user_role.model.js
@@ -28,9 +28,14 @@ var Models = {
             let sql = `insert into user_role (user_ro_description) values (${dbconnection.escape(data.user_ro_description)})`
 
             dbconnection.query(sql, (error, rows) =>{
-                if(error) throw error
+                if(error) {
+                    console.error('Error al insertar el usuario rol', error)
+                    return callback({ error: 'No se puedo crear el usuario rol'})
+                }
                 callback({message : 'Usuario rol creado satisfactoriamente'})
             })
+        } else {
+            console.error('La conexión a la base de datos no está disponible')
         }
     }
 }
