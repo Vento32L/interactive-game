895e3f15f09b66c67208b76b1c838b74b58d8685
lorelia1818 <laroca94@uan.edu.co>
2023-10-02 23:15:31 -0500

    Update

--- Backend-InteractiveGame-NasaYuwe/app/routes/game.route.js
+++ Backend-InteractiveGame-NasaYuwe/app/routes/game.route.js
@@ -4,8 +4,8 @@ const route = require('express').Router()
 const {getGames, getOneGame, addGame}
  = require('../controllers/game.controller')
 
-route.route('/Games').get(getGames)
-route.route('/Game/:game_id').get(getOneGame)
-route.route('/Game').post(addGame)
+route.route('/games').get(getGames)
+route.route('/games/:game_id').get(getOneGame)
+route.route('/Games').post(addGame)
 
 module.exports = route
--- Backend-InteractiveGame-NasaYuwe/app/routes/game_type.route.js
+++ Backend-InteractiveGame-NasaYuwe/app/routes/game_type.route.js
@@ -5,7 +5,7 @@ const {getGame_Types, getOneGame_Type, addGame_Type}
  = require('../controllers/game_type.controller')
 
 route.route('/game_types').get(getGame_Types)
-route.route('/game_type/:game_ty_id').get(getOneGame_Type)
-route.route('/game_type').post(addGame_Type)
+route.route('/game_types/:game_ty_id').get(getOneGame_Type)
+route.route('/game_types').post(addGame_Type)
 
 module.exports = route
